022473e956bddb3d077f16a7740a71e76105c732
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-07-23 00:42:11 +0300

    feat: add membertype to schema

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,14 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { RootQuery, createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { GraphQLSchema, graphql } from 'graphql';
+
+const mySchema: GraphQLSchema = new GraphQLSchema({
+  query: RootQuery,
+});
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma, httpErrors } = fastify;
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,9 +19,18 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      console.log(req.body.query);
-      console.log(req.body.variables);
-      return {};
+      //TODO: вызов graphql сделать с нужными параметрами (схема, текст запроса, переменные)
+      // console.log(req.body.query);
+      // console.log(req.body.variables);
+
+      const query = req.body.query;
+
+      const result = await graphql({
+        schema: mySchema,
+        source: query,
+        contextValue: fastify,
+      });
+      return result;
     },
   });
 };
--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLFloat, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLString, GraphQLNonNull } from 'graphql';
+import { MemberType } from '@prisma/client';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +23,38 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const MemberType = new GraphQLObjectType({
+  name: 'memberType',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    discount: { type: GraphQLFloat },
+    postsLimitPerMonth: { type: GraphQLInt },
+  }),
+});
+
+export const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    memberType: {
+      type: MemberType,
+      args: { id: { type: GraphQLString } },
+      resolve: async (_, args: { id: string }, context) => {
+        const memberType: MemberType | null = await context.prisma.memberType.findUnique({
+          where: {
+            id: args.id,
+          },
+        })
+        return memberType;
+      }
+    },
+    memberTypes: {
+      type:  new GraphQLList(MemberType),
+      args: {},
+      resolve: async (_, args, context) => {
+        const memberTypes: Array<MemberType> | Array<null> = await context.prisma.memberType.findMany();
+        return memberTypes;
+      }
+    }
+  }
+});
