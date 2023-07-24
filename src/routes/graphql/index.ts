e1401d28935e0ca776d0e4ba37548e375eadc1fb
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-07-24 22:51:34 +0300

    feat: add depthlimit validation (subtask 3)

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { RootQuery, Mutation, createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { GraphQLSchema, graphql } from 'graphql';
+import { GraphQLSchema, graphql, validate, parse } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const mySchema: GraphQLSchema = new GraphQLSchema({
   query: RootQuery,
@@ -19,9 +20,23 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
+    async handler(req, rep) {
       const query = req.body.query;
       const variables = req.body.variables;
+
+      try {
+        const errorsList = validate(mySchema, parse(query), [ depthLimit(5) ]);
+        if (errorsList.length > 0) {
+          throw new Error();
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          return await rep.send({
+            errors: [ { message: 'exceeds maximum operation depth of 5' } ],
+          });
+        }
+      }
+
 
       const result = await graphql({
         schema: mySchema,
